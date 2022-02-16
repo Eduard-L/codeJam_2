6ea4ad85eff7d2b25f56df1deafccc3508b3d26b
iliazaidin <ilia.zaid@gmail.com>
2022-02-16 17:43:40 +0200

    fix/reverse after bad merge

--- src/components/Card/Card.js
+++ src/components/Card/Card.js
@@ -1,30 +1,6 @@
-import { PopupColorPicker } from '../PopupColorPicker/PopupColorPicker';
-import { useDrop } from 'react-dnd';
-import { initialImages } from '../../utils/costants';
-
-export function Card({ setInitialData, initialData, droppedImages, onDrop, cardBackgroundColor, isPopupColorPickerOpen, onColorPickerSubmit, children }) {
-  function handleDropItem(id) {
-    const image = initialImages.find((image) => image.id === id)
-    image.drag = true;
-    onDrop([image, ...droppedImages])
-    setInitialData(initialData.filter((image) => image.id !== id))
-
-  }
-
-  const [{ isOver }, dropRef] = useDrop({
-    accept: 'Image',
-    drop: (item) => handleDropItem(item.id),
-    collect: (monitor) => ({
-      isOver: !monitor.isOver(),
-    }),
-  })
-
+export function Card({ id, cardBackgroundColor }) {
   return (
-    <div className='card' role={'container'} ref={dropRef} style={{ backgroundColor: isOver ? `${cardBackgroundColor}` : 'violet' }}>
-      <PopupColorPicker isPopupColorPickerOpen={isPopupColorPickerOpen} onColorPickerSubmit={onColorPickerSubmit} />
-      {children}
+    <div className='card' id={id} style={{ backgroundColor: `${cardBackgroundColor}` }}>
     </div >
   )
 }
-
-// backgroundColor: `${cardBackgroundColor}` 
--- src/components/Main/Main.js
+++ src/components/Main/Main.js
@@ -1,47 +1,32 @@
 import { Card } from '../Card/Card'
 import { Button } from '../Button/Button';
-
+import { PopupWithImages } from '../PopupWithImages/PopupWithImages'
 import { initialImages } from "../../utils/costants";
 import { Image } from "../Image/Image";
-import { useState } from 'react';
+import { PopupColorPicker } from '../PopupColorPicker/PopupColorPicker';
 
-export function Main({ onTextButtonClick, onColorButtonClick, isPopupColorPickerOpen, onColorPickerSubmit, cardBackgroundColor }) {
-  const [initialData, setInitialData] = useState(initialImages)
-  const [droppedImages, setDroppedImages] = useState([])
-
-  function handleDropImageClick(id) {
-
-    const deleteImage = droppedImages.find((image) => image.id === id)
-    setDroppedImages(droppedImages.filter((image) => image !== deleteImage))
-    setInitialData(initialData.concat(deleteImage))
-
-
-  }
+export function Main({ isOpen, setIsPopupWithImgOpen, onImageClick, onTextButtonClick, onColorPickerSubmit, cardBackgroundColor }) {
   return (
     <div className="Main">
 
-      <Card setInitialData={setInitialData} initialData={initialData} droppedImages={droppedImages} onDrop={setDroppedImages} cardBackgroundColor={cardBackgroundColor} isPopupColorPickerOpen={isPopupColorPickerOpen} onColorPickerSubmit={onColorPickerSubmit}>
-
-        {
-          droppedImages.map((image) => {
-            return (
-              <Image onImageClick={handleDropImageClick} src={image.src} key={image.id} alt={image.alt} id={image.id} />
-            )
-          })
-        }
-
-      </Card>
+      <Card id="div1" cardBackgroundColor={cardBackgroundColor}/>
 
       <div className='button-wrapper'>
         <Button classType='button' text='Text' onButtonClick={onTextButtonClick} />
-        <Button classType='button' text='Color' onButtonClick={onColorButtonClick} />
-        {initialData.map((image) => {
+        <PopupColorPicker onColorPickerSubmit={onColorPickerSubmit} />
+        {initialImages.map((image) => {
           return (
-            <Image src={image.src} key={image.id} alt={image.alt} id={image.id} onImageClick={() => { }} />
+            <Image src={image.src} key={image.id} alt={image.alt} id={image.id} onImageClick={onImageClick} />
           )
         })}
       </div>
-
+      {/* <PopupWithImages isOpen={isOpen} >
+        {initialImages.map((image) => {
+          return (
+            <Image src={image.src} key={image.id} alt={image.alt} id={image.id} onImageClick={onImageClick} />
+          )
+        })}
+      </PopupWithImages> */}
     </div>
   )
 }
